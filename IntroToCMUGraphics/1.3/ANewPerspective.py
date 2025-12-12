93862c40e40282b1da7f9fdf9c053910de8547e4
Legendaryistic <163098152+Legendaryistic@users.noreply.github.com>
2025-12-12 13:27:17 -0800

    Create ANewPerspective.py

---
+++ IntroToCMUGraphics/1.3/ANewPerspective.py
@@ -0,0 +1,22 @@
+# background
+Rect(0, 0, 400, 400, fill=gradient('white', 'navy'))
+
+# Draw the perspective lines next.
+Line(100,0,100,250)
+Line(100,250,0,400)
+Line(100,250,300,250)
+Line(300,250,400,400)
+Line(300,250,300,0)
+### Place Your Code Here ###
+
+Oval(200,315,100,30)
+# Draw the ninja here.
+Circle(200,200,70,fill=gradient('lightBlue','royalBlue'))
+Line(160,165,190,170,lineWidth=3)
+Line(210,170,240,165,lineWidth=3)
+Oval(200,195,140,50)
+Oval(175,195,30,40,fill='white')
+Oval(225,195,30,40,fill='white')
+Circle(222,195,10)
+Circle(178,195,10)
+Polygon(270,195,280,180,290,190)
