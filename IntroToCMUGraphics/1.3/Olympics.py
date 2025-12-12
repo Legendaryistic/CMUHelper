e171d942d34196e2be83f97bd58e382de889d146
Legendaryistic <163098152+Legendaryistic@users.noreply.github.com>
2025-12-12 13:25:52 -0800

    Create Olympics.py

---
+++ IntroToCMUGraphics/1.3/Olympics.py
@@ -0,0 +1,11 @@
+# Draw the five Olympics Rings.
+### (HINT: Pay special attention to the order in which the shapes are drawn!)
+### Place Your Code Here ###
+Circle(75,160, 60, borderWidth = 10, border = 'dodgerBlue', fill = None)
+Circle(140,220,60,borderWidth = 10, border = 'orange', fill = None)
+Circle(200,160,60,borderWidth = 10, border = 'black', fill = None)
+Circle(270,220,60,borderWidth = 10, border = 'forestGreen', fill = None)
+Circle(325,160,60,borderWidth = 10, border = 'red', fill = None)
+# Draw the text at the bottom.
+Label('The Olympics',200,320, fill = 'red', font = 'arial', size = 50, bold = True)
+### Place Your Code Here ###
