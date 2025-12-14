0f0281b414ed0f6f4b0efbce5de297b73ef64e40
Legendaryistic <163098152+Legendaryistic@users.noreply.github.com>
2025-12-13 16:48:26 -0800

    Create ShinyLines.py

---
+++ FunctionsMouseEventsConditionals/MouseEvents2.2/2.2.2/ShinyLines.py
@@ -0,0 +1,9 @@
+# background
+Rect(0, 0, 400, 400)
+
+def onMousePress(mouseX, mouseY):
+    # Draw a line from left-top corner to where the mouse is being clicked.
+    Line(0,0,mouseX,mouseY, fill = gradient('red','black'), lineWidth = 2)
+    ### (HINT: Don't forget about the gradient!)
+    ### Place Your Code Here ###
+    pass
