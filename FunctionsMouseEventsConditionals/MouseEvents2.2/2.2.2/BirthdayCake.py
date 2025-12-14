6e83762a06e351264d7c6ebb836134be1bb59cea
Legendaryistic <163098152+Legendaryistic@users.noreply.github.com>
2025-12-13 16:48:41 -0800

    Create BirthdayCake.py

---
+++ FunctionsMouseEventsConditionals/MouseEvents2.2/2.2.2/BirthdayCake.py
@@ -0,0 +1,28 @@
+# background
+Rect(0, 0, 400, 150, fill='lightSteelBlue')
+Rect(0, 150, 400, 250, fill='steelBlue')
+
+# plate
+Oval(200, 260, 360, 200, fill='lavender')
+
+# cake
+Oval(200, 240, 300, 170, fill='tan')
+Rect(50, 200, 300, 40, fill='tan')
+Oval(200, 200, 300, 170, fill='wheat')
+Rect(50, 160, 300, 40, fill='wheat')
+Oval(200, 160, 300, 170, fill='saddleBrown')
+
+def onMousePress(mouseX, mouseY):
+    Line(mouseX,mouseY, mouseX,mouseY-50, lineWidth = 8, fill = gradient('paleVioletRed', 'pink', start = 'bottom'))
+    # Draw a candle.
+    ### Place Your Code Here ###
+    pass
+
+def onMouseRelease(mouseX, mouseY):
+    Circle(mouseX,mouseY-50, 5, fill = 'lightYellow')
+    Polygon(mouseX-5, mouseY-50, mouseX+5, mouseY-50, mouseX, mouseY-60, fill = 'lightYellow')
+    # Draw a flame for the candle.
+    ### (HINT: A flame is a circle and a triangle. Which shape should be used
+    #          to draw a triangle? Use the inspector tool to find the points!)
+    ### Place Your Code Here ###
+    pass
