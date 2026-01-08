cc580e399956c5541b1ec6ec48b888db433ed9a6
Legendaryistic <163098152+Legendaryistic@users.noreply.github.com>
2026-01-08 15:22:48 -0800

    Create 4.1.2 Checkpoint.py

---
+++ ComplexConditionalsMoreEventsandLibraries/4.1.2 Checkpoint.py
@@ -0,0 +1,18 @@
+app.background = 'black'
+
+Line(200, 0, 200, 400, fill='lightGrey')
+Line(0, 200, 400, 200, fill='lightGrey')
+
+def onMousePress(mouseX, mouseY):
+    if (mouseX < 200):
+        if mouseY < 200:
+            Star(mouseX, mouseY, 30, 7, fill = 'springGreen')
+        
+        else:
+            Circle(mouseX, mouseY, 35, fill = 'fuchsia')
+            Label("Woohoo!", mouseX, mouseY)
+       
+        
+
+    else:
+        RegularPolygon(mouseX, mouseY, 25, 6, fill='orange')
